4ca2ceec540dccb5cf932ede87182d93f81afda2
Dilip Kosuri <dilip.kosuri@gmail.com>
2022-08-19 14:05:30 +0200

    Add files via upload
    First draft version

---
+++ SwiftUIApp/SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Dilip Kosuri on 2022-08-19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
